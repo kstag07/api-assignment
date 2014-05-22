be99f16fa703725a1a1b6603da77f9ccd637d151
Kevin Staiger <kstag07@yahoo.com>
2014-05-22 14:25:11 -0700

    resubmitting assignment

--- wordinator.rb
+++ wordinator.rb
@@ -1,14 +1,10 @@
 require 'sinatra'
 require 'json'
 
-# get '/:name/:city' do
-#   "#{params[:name]} and #{params[:city]}"
-# end
-
 get '/:key1/:key2' do
   content_type :json
   value1 = params[:key1]
   value2 = params[:key2]
-   f = value1.to_s.chars.sort == value2.to_s.chars.sort
-  {:word1 => value1, :word2 => value2, anagram?: f}.to_json
+  anagram = value1.to_s.chars.sort == value2.to_s.chars.sort
+  {:word1 => value1, :word2 => value2, anagram?: anagram}.to_json
 end
